e37cbdb020959d644828420b2e9d282e6a0d8b71
Maqbool ur Rahim Khan <maqboolurrahim@gmail.com>
2026-09-12 13:16:28 +0200

    Release 1.0.0-alpha.10

--- Package.swift
+++ Package.swift
@@ -17,13 +17,13 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Primordial",
-            url: "https://github.com/fairymeadowslab/primordial-apple/releases/download/1.0.0-alpha.9/Primordial.xcframework.zip",
-            checksum: "12cb19bd719e7586639e4d4b9e575128bc14a50349213ec6d1334f61c3c4cb65"
+            url: "https://github.com/fairymeadowslab/primordial-apple/releases/download/1.0.0-alpha.10/Primordial.xcframework.zip",
+            checksum: "826540bef3ecf5b845fda3919a9b9901b068675571c674cac1510e9129fa6e64"
         ),
         .binaryTarget(
             name: "ObjectBox",
-            url: "https://github.com/fairymeadowslab/primordial-apple/releases/download/1.0.0-alpha.9/ObjectBox.xcframework.zip",
-            checksum: "3494f8f6c99a9b12012324e260c552149a1a37ee06293c018f47a9490f6a68ab"
+            url: "https://github.com/fairymeadowslab/primordial-apple/releases/download/1.0.0-alpha.10/ObjectBox.xcframework.zip",
+            checksum: "e326df64af11c964fcff096bc8d177070dc8f3b40aace8ba584c6682dfc862d4"
         )
     ]
 )
